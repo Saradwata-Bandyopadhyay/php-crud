13b4d64a1de0d9f03961a64a603b38fc696da3bb
Saradwata-Bandyopadhyay <43028785+Saradwata-Bandyopadhyay@users.noreply.github.com>
2023-10-29 18:55:07 +0530

    Update create.php
    Create code updated.

--- create.php
+++ create.php
@@ -1,1 +1,23 @@
-// update the create code
+<?php
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['task'])) {
+    $task = $_POST['task'];
+
+    try {
+        $stmt = $pdo->prepare("INSERT INTO tasks (task) VALUES (:task)");
+        $stmt->bindParam(':task', $task);
+
+        if ($stmt->execute()) {
+            header("Location: index.php");
+            exit(); 
+        } else {
+            echo "Error in data insertion: " . implode(", ", $stmt->errorInfo());
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    echo "Task data not provided or invalid request.";
+}
+?>
